3450356fccf697df9a63de6f0e7a21a5c34661d4
hjg <richardgong1988@gmail.com>
2025-07-16 15:58:40 +0900

    fixed start up error

--- server/initialize/plugin_biz_v1.go
+++ server/initialize/plugin_biz_v1.go
@@ -29,6 +29,7 @@ func bizPluginV1(group ...*gin.RouterGroup) {
 		global.GVA_CONFIG.Email.Nickname,
 		global.GVA_CONFIG.Email.Port,
 		global.GVA_CONFIG.Email.IsSSL,
+		global.GVA_CONFIG.Email.IsLoginAuth,
 	))
 	holder(public, private)
 }
