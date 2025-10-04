852f24c43ffe621d848a4e42b2068b3d63f5a0cc
hjg <richardgong1988@gmail.com>
2025-10-04 22:48:57 +0900

    updated package

---
+++ gong-nextjs-ui/src/app/admin/laptop/page.tsx
@@ -0,0 +1,5 @@
+import LaptopManagementTable from "@/components/admin/laptop.component";
+
+export default function LaptopPage() {
+    return <LaptopManagementTable />; // uses defaultService internally
+}
